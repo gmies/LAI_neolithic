b81e2d69fdc28571dfa6c3aaa78a428967245ff8
gmies <89655422+gmies@users.noreply.github.com>
2026-04-01 13:38:33 -0400

    Update run_ancestry_hmm_posterior_filter.sh

--- run_LAI/run_ancestry_hmm_posterior_filter.sh
+++ run_LAI/run_ancestry_hmm_posterior_filter.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+#script to take ancestry hmm output and put into rfmix format and then calculate LA for admixed pop and global ancestry for each admixed individual 
+
 module load R/4.4
-Rscript /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/11_12_25_run_samplesizes/scripts/format/filter_ancestry_hmm_format.R
-Rscript /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/selection_analysis/same_format_042125/scripts/rfmix_output_to_mean.R ./output_ancestryhmm_ /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/mosaic/rates.
-Rscript /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/selection_analysis/same_format_042125/scripts/rfmix_global.R ./output_ancestryhmm_ 
+Rscript LAI_analyses/scripts/filter_ancestry_hmm_format.R
+Rscript LAI_analyses/scripts/rfmix_output_to_mean.R ./output_ancestryhmm_ /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/mosaic/rates.
+Rscript LAI_analyses/scripts/rfmix_global.R ./output_ancestryhmm_ 
 
 
