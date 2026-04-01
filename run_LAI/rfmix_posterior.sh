dcd6a16a9b2c970a4ab7692c27638dcca6382554
gmies <89655422+gmies@users.noreply.github.com>
2026-04-01 13:33:27 -0400

    Update rfmix_posterior.sh

--- run_LAI/rfmix_posterior.sh
+++ run_LAI/rfmix_posterior.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 
+# rfmix_posterior.sh
+#-------------------------------------------------------------
+# Runs RFMix for chromosomes 1-22 and filters posterior 
+# probabilities (>0.9) using a local R script.
+#-------------------------------------------------------------
+# Expected directory structure:
+#   ./data/PopPhased/                        # phased input for RFMix
+#   ./data/rfmix_alleles1.txt ... rfmix_alleles22.txt
+#   ./data/classes.txt
+#   ./data/markerLocationsChr1.txt ... markerLocationsChr22.txt
+#   ./data/rates.1 ... rates.22              # genetic map / recombination rates
+#   ./data/simplai_global_ancestry.txt       # sample global ancestry info
+#   ./scripts/posterior_filtering.R          # R script to filter posterior
+#-------------------------------------------------------------
+# Usage:
+#   bash rfmix_posterior.sh
+
+# Load required modules
 module load R/4.4
-
-cp -r /project/mathilab/aaw/Mitonuclear2/data/rfmix/PopPhased/ .
-cp /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/11_12_25_run_samplesizes/7v7/rfmix_format/rfmix/filter_posterior/RunRFMix.py .
-
-
 module load bcftools/1.21  
 module load plink/2.0-20210505
 module load jre/1.8.0_211
 module load python/3.8
 
+# Loop over chromosomes 1-22 and run RFMix
 for chr in {1..22}
 do
-#python2 /project/mathilab/aaw/Mitonuclear2/data/rfmix/RunRFMix.py PopPhased ../../../rfmix/rfmix_alleles${chr}.txt ../../../rfmix/classes.txt /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/rfmix_sep/markerLocationsChr${chr}.txt -G $input_gen -n 5  --forward-backward -o output_rfmix_${chr}
+    echo "Running RFMix for chromosome $chr..."
 
-python3 RunRFMix.py PopPhased ../../../rfmix/rfmix_alleles${chr}.txt ../../../rfmix/classes.txt /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/rfmix_sep/markerLocationsChr${chr}.txt -G 35 -n 5  --forward-backward -o output_rfmix_${chr}
-
+    python3 RunRFMix.py data/PopPhased \
+        data/rfmix_alleles${chr}.txt \
+        data/classes.txt \
+        data/markerLocationsChr${chr}.txt \
+        -G 35 -n 5 --forward-backward -o output_rfmix_${chr}
 done
 
-mkdir output
+# Create output folder for filtered files
+mkdir -p output
 cd output
-Rscript /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/11_12_25_run_samplesizes/scripts/combined_analysis/posterior_filtering.R /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/mosaic/rates. /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/GA/simplai_7/simplai_global_ancestry.txt
 
-#Rscript /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/11_12_25_run_samplesizes/posterior_filtering.R /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/mosaic/rates. /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/GA/simplai_7/simplai_global_ancestry.txt
+# Run posterior probability filtering using local R script
+Rscript ../scripts/posterior_filtering.R \
+    ../data/rates. \
+    ../data/simplai_global_ancestry.txt
