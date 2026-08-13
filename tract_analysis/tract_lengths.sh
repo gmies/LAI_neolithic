b774a5ea4841ac498adad3bf945467df3e1932dd
gmies <89655422+gmies@users.noreply.github.com>
2026-08-13 09:46:20 -0400

    Update tract_lengths.sh

--- tract_analysis/tract_lengths.sh
+++ tract_analysis/tract_lengths.sh
@@ -33,10 +33,28 @@ python  /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_
 --output-prefix mosaic
 
 
+#gnomix:
+cd ../gnomix
+cd /project/mathilab/gmies/neolithic_selection/1kg_runs/results/rfmix_format_022226/gnomix/
+python  /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/selection_analysis/same_format_042125/tract_lengths/analyze_rfmix_tracts.py \
+--hmm-file /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/ancestry_hmm/output_hmm_run_file \
+--rfmix-pattern "output_gnomix_\${chr}.txt.gz" \
+--output-prefix gnomix
+
+
+#recombmix:
+cd ../recombmix
+cd /project/mathilab/gmies/neolithic_selection/1kg_runs/results/rfmix_format_022226/recombmix/
+python  /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/selection_analysis/same_format_042125/tract_lengths/analyze_rfmix_tracts.py \
+--hmm-file /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/results/ancestry_hmm/output_hmm_run_file \
+--rfmix-pattern "output_recombmix_\${chr}.txt.gz" \
+--output-prefix recombmix
+
+
 #previously from /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/selection_analysis/same_format_042125/tract_lengths
 
 #plot hg tracts together from each method 
-Rscript /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/selection_analysis/same_format_042125/tract_lengths/theoretical/add_theoretical_tracts_5.R 4 hg ../ancestry_hmm/ancestry_hmm_tract_lengths_data.txt ../simplai/simplai_tract_lengths_data.txt ../rfmix/rfmix_tract_lengths_data.txt ../mosaic/mosaic_tract_lengths_data.txt ancestry_hmm simplai rfmix mosaic 
+Rscript /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/selection_analysis/same_format_042125/tract_lengths/theoretical/add_theoretical_tracts_5.R 6 hg ../ancestry_hmm/ancestry_hmm_tract_lengths_data.txt ../simplai/simplai_tract_lengths_data.txt ../rfmix/rfmix_tract_lengths_data.txt ../mosaic/mosaic_tract_lengths_data.txt ../recombmix/recombmix_tract_lengths_data.txt ../gnomix/gnomix_tract_lengths_data.txt ancestry_hmm simplai rfmix mosaic recombmix gnomix 
 
 #plot farmer tracts together from each method
-Rscript /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/selection_analysis/same_format_042125/tract_lengths/theoretical/add_theoretical_tracts_5.R 4 farmer ../ancestry_hmm/ancestry_hmm_tract_lengths_data.txt ../simplai/simplai_tract_lengths_data.txt ../rfmix/rfmix_tract_lengths_data.txt ../mosaic/mosaic_tract_lengths_data.txt ancestry_hmm simplai rfmix mosaic 
+Rscript /project/mathilab/gmies/neolithic_selection/allentoft_data/031825_rerun_filter_allen_LAI/selection_analysis/same_format_042125/tract_lengths/theoretical/add_theoretical_tracts_5.R 6 farmer ../ancestry_hmm/ancestry_hmm_tract_lengths_data.txt ../simplai/simplai_tract_lengths_data.txt ../rfmix/rfmix_tract_lengths_data.txt ../mosaic/mosaic_tract_lengths_data.txt ../recombmix/recombmix_tract_lengths_data.txt ../gnomix/gnomix_tract_lengths_data.txt ancestry_hmm simplai rfmix mosaic recombmix gnomix 
